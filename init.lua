2a111c4530badcbe356b2ffe7be0127082667c47
cyrillus <kirill.olegovich31@gmail.com>
2025-06-23 22:09:03 +0300

    treesitter: install nvim-treesitter

--- init.lua
+++ init.lua
@@ -5,6 +5,7 @@ require("config.lazy")
 vim.opt.shiftwidth = 4
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.clipboard = "unnamedplus"
 
 vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
 
