c72d12d13fba05b0087141e0b8e7d1d01c13fde3
cyrillus <kirill.olegovich31@gmail.com>
2025-06-30 21:43:34 +0300

    mini-statusline: turn it off in NvimTree and Oil

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,13 +1,52 @@
 -- https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-statusline.md
-return { 
-  {
-    'echasnovski/mini.nvim', 
-    enable = true,
-    version = false ,
-    config = function()
-      -- NOTE: It is advised to call require("mini.statusline").setup()
-      local statusline = require("mini.statusline")
-      statusline.setup( { use_icons = true } )
-    end,
-  }
+
+local default_active_config = function()
+	if vim.bo.filetype == "NvimTree" then
+		return " "
+	end
+	local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
+	local git           = MiniStatusline.section_git({ trunc_width = 40 })
+	local diff          = MiniStatusline.section_diff({ trunc_width = 75 })
+	local diagnostics   = MiniStatusline.section_diagnostics({ trunc_width = 75 })
+	local lsp           = MiniStatusline.section_lsp({ trunc_width = 75 })
+	local filename      = MiniStatusline.section_filename({ trunc_width = 140 })
+	local fileinfo      = MiniStatusline.section_fileinfo({ trunc_width = 120 })
+	local location      = MiniStatusline.section_location({ trunc_width = 75 })
+	local search        = MiniStatusline.section_searchcount({ trunc_width = 75 })
+
+	return MiniStatusline.combine_groups({
+		{ hl = mode_hl,                 strings = { mode } },
+		{ hl = 'MiniStatuslineDevinfo', strings = { git, diff, diagnostics, lsp } },
+		'%<', -- Mark general truncate point
+		{ hl = 'MiniStatuslineFilename', strings = { filename } },
+		'%=', -- End left alignment
+		{ hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
+		{ hl = mode_hl,                  strings = { search, location } },
+	})
+end
+
+
+return {
+	{
+		'echasnovski/mini.nvim',
+		enable = true,
+		version = false,
+		config = function()
+			-- NOTE: It is advised to call require("mini.statusline").setup()
+			local statusline = require("mini.statusline")
+			statusline.setup({
+				content = {
+					-- Content for active window. If nil - use default.
+					active = function()
+						if vim.bo.filetype == "NvimTree" or vim.bo.filetype == "oil" then
+							return " "
+						end
+						return default_active_config()
+					end,
+					inactive = nil,
+				},
+				use_icons = true,
+			})
+		end,
+	}
 }
