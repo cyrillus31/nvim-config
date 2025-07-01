bde02815ab92be9aabe0cb8a2bb61c1b87f707d6
cyrillus <kirill.olegovich31@gmail.com>
2025-07-01 03:38:02 +0300

    fix(autoformatting): change conform's setup on save to false instead of nil

--- lua/plugins/autoformat.lua
+++ lua/plugins/autoformat.lua
@@ -15,7 +15,7 @@ return {
 		},
 		opts = {
 			notify_on_error = false,
-			format_on_save = nil,
+			format_on_save = false,
 			-- format_on_save = function(bufnr)
 			--   -- Disable "format_on_save lsp_fallback" for languages that don't
 			--   -- have a well standardized coding style. You can add additional
