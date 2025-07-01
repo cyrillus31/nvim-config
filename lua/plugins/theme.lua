9cfe3ff1c904c04a84c37570119ef65ec837a3e3
cyrillus <kirill.olegovich31@gmail.com>
2025-07-01 14:01:50 +0300

    themese: add different color schemes

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,3 +1,42 @@
 return {
-	{ "folke/tokyonight.nvim", config = function() vim.cmd.colorscheme("tokyonight") end },
+	{
+		"folke/tokyonight.nvim",
+		enabled = false,
+		config = function()
+			vim.cmd.colorscheme("tokyonight")
+		end,
+	},
+	{
+		"catppuccin/nvim",
+		enabled = false,
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("catppuccin-mocha")
+		end,
+	},
+	{
+		"rebelot/kanagawa.nvim",
+		enabled = true,
+		config = function()
+			vim.cmd.colorscheme("kanagawa-wave")
+		end,
+	},
+	{
+		"ellisonleao/gruvbox.nvim",
+		enabled = false,
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("gruvbox")
+		end,
+		opts = {},
+	},
+	{
+		"projekt0n/github-nvim-theme",
+		enabled = false,
+		name = "github-theme",
+		config = function()
+			vim.cmd.colorscheme("github_dark_dimmed")
+		end,
+	},
 }
