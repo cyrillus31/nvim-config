01a4aed5bfd87baa77b9fad46dfb87b608d7172b
cyrillus <kirill.olegovich31@gmail.com>
2025-07-04 23:27:42 +0300

    plugin(statusline): add lualine plugin and adjust mini.statusline

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,110 @@
+-- Tabs helper object
+local T = {}
+
+T.tabnames = {}
+
+function T.getCurrentTabIndex()
+	local tab_index = vim.api.nvim_tabpage_get_number(vim.api.nvim_get_current_tabpage())
+	return tab_index
+end
+
+function T.getTabName()
+	local cur = T.getCurrentTabIndex()
+	return tostring(T.tabnames[cur]) or ""
+end
+
+function T.changeTabname()
+	T.tabnames[tostring(T.getCurrentTabIndex())] = vim.fn.input("Tab rename: ")
+	for key, value in pairs(T.tabnames) do
+		print(key .. "and" .. value)
+	end
+end
+
+function T.set_keymaps()
+	vim.keymap.set("n", "vtr", T.changeTabname, { desc = "Tab rename" })
+end
+
+return {
+	"nvim-lualine/lualine.nvim",
+	enabled = true,
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local function get_python_env()
+			-- ' '
+			local command = "env=$VIRTUAL_ENV_PROMPT; if [[ ! -z $env ]]; then echo $env; else echo ''; fi"
+			local output = vim.fn.system(command)
+			return string.sub(output, 1, -2)
+		end
+
+		require("lualine").setup({
+			options = {
+				icons_enabled = true,
+				theme = "auto",
+				-- theme = 'wombat',
+				-- theme = 'gruvbox',
+				-- theme = 'everforest',
+				-- theme = 'palenight',
+				--
+				--
+				--
+				--
+				-- component_separators = { left = '', right = '' },
+				component_separators = { left = "", right = "" },
+				-- component_separators = { left = '', right = '' }
+				-- section_separators = { left = '', right = '' },
+				-- section_separators = { left = '', right = '' },
+				-- section_separators = { left = '', right = '' },
+				-- section_separators = { left = ' ', right = ' ' },
+				-- section_separators = { left = ' ', right = ' ' },
+				-- section_separators = { left = ' ', right = '' },
+				section_separators = { left = "", right = " " },
+				disabled_filetypes = {
+					statusline = { "NvimTree", "Neotree" },
+					winbar = {},
+				},
+				ignore_focus = { "NvimTree", "Neotree" },
+				always_divide_middle = true,
+				globalstatus = false,
+				refresh = {
+					statusline = 1000,
+					tabline = 1000,
+					winbar = 1000,
+				},
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff" },
+				lualine_c = { "diagnostics" },
+				-- lualine_c = { 'filename' },
+				lualine_x = { "fileformat", "filetype", get_python_env },
+				-- lualine_x = { 'encoding', 'fileformat', 'filetype' },
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = {},
+				lualine_x = { "filename", "location" },
+				lualine_y = {},
+				lualine_z = {},
+			},
+			-- below is the line for tabs
+			-- tabline = {
+			-- 	lualine_a = { "tabs" },
+			-- 	lualine_b = { T.getTabName() },
+			-- 	-- lualine_a = { 'tabs', separator = { left = '', right = ''}, cond = function () return true end },
+			-- 	lualine_c = { "filename" },
+			-- },
+			-- winbar = {
+			--   lualine_a = { 'filename' },
+			--   -- lualine_b = { 'diagnostics' },
+			-- },
+			inactive_winbar = {
+				-- lualine_b = { 'filename' },
+				-- lualine_c = { 'diagnostics' },
+			},
+			extensions = {},
+		})
+	end,
+}
--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,36 +1,42 @@
 -- https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-statusline.md
 
 -- You can find the original config in :help ministatusline
-local default_config = function()
-	if vim.bo.filetype == "NvimTree" then
+local modified_config = function()
+	local ignore_filetype = {
+		["NvimTree"] = true,
+		["neo-tree"] = true,
+		["oil"] = true,
+	}
+
+	if ignore_filetype[vim.bo.filetype] then
 		return " "
 	end
+
 	local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
-	local git           = MiniStatusline.section_git({ trunc_width = 40 })
-	local diff          = MiniStatusline.section_diff({ trunc_width = 75 })
-	local diagnostics   = MiniStatusline.section_diagnostics({ trunc_width = 75 })
-	local lsp           = MiniStatusline.section_lsp({ trunc_width = 75 })
-	local filename      = MiniStatusline.section_filename({ trunc_width = 140 })
-	local fileinfo      = MiniStatusline.section_fileinfo({ trunc_width = 120 })
-	local location      = MiniStatusline.section_location({ trunc_width = 75 })
-	local search        = MiniStatusline.section_searchcount({ trunc_width = 75 })
+	local git = MiniStatusline.section_git({ trunc_width = 40 })
+	local diff = MiniStatusline.section_diff({ trunc_width = 75 })
+	local diagnostics = MiniStatusline.section_diagnostics({ trunc_width = 75 })
+	local lsp = MiniStatusline.section_lsp({ trunc_width = 75 })
+	local filename = MiniStatusline.section_filename({ trunc_width = 140 })
+	local fileinfo = MiniStatusline.section_fileinfo({ trunc_width = 120 })
+	local location = MiniStatusline.section_location({ trunc_width = 75 })
+	local search = MiniStatusline.section_searchcount({ trunc_width = 75 })
 
 	return MiniStatusline.combine_groups({
-		{ hl = mode_hl,                 strings = { mode } },
-		{ hl = 'MiniStatuslineDevinfo', strings = { git, diff, diagnostics, lsp } },
-		'%<', -- Mark general truncate point
-		{ hl = 'MiniStatuslineFilename', strings = { filename } },
-		'%=', -- End left alignment
-		{ hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
-		{ hl = mode_hl,                  strings = { search, location } },
+		{ hl = mode_hl, strings = { mode } },
+		{ hl = "MiniStatuslineDevinfo", strings = { git, diff, diagnostics, lsp } },
+		"%<", -- Mark general truncate point
+		{ hl = "MiniStatuslineFilename", strings = { filename } },
+		"%=", -- End left alignment
+		{ hl = "MiniStatuslineFileinfo", strings = { fileinfo } },
+		{ hl = mode_hl, strings = { search, location } },
 	})
 end
 
-
 return {
 	{
-		'echasnovski/mini.nvim',
-		enable = true,
+		"echasnovski/mini.nvim",
+		enable = false,
 		version = false,
 		config = function()
 			-- NOTE: It is advised to call require("mini.statusline").setup()
@@ -39,15 +45,12 @@ return {
 				content = {
 					-- Content for active window. If nil - use default.
 					active = function()
-						if vim.bo.filetype == "NvimTree" or vim.bo.filetype == "oil" then
-							return " "
-						end
-						return default_config()
+						return modified_config()
 					end,
 					inactive = nil,
 				},
 				use_icons = true,
 			})
 		end,
-	}
+	},
 }
