ae9aa67e0a3d5d784e590ef3d7fb19342fd23194
cyrillus <kirill.olegovich31@gmail.com>
2025-07-05 02:24:06 +0300

    plugin(bugged): add vimcaps placeholder just in case

---
+++ lua/plugins/vimcaps.lua
@@ -0,0 +1,6 @@
+-- This plug manages CapsLock for you and turns it off when exeting insert mode
+-- WARN: It's very useful but I wasn't able to configure it run it on Fedora or Mac.
+-- https://github.com/suxpert/vimcaps
+--
+-- return {'suxpert/vimcaps'}
+return {}
