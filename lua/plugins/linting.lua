64185ef932b25b90ba709839f937c801cf996216
cyrillus <kirill.olegovich31@gmail.com>
2025-07-05 22:30:55 +0300

    refactor(lint): improve how linting configuration is structured

--- lua/plugins/linting.lua
+++ lua/plugins/linting.lua
@@ -1,13 +1,16 @@
 -- Key mapping to toggle linting (e.g., <leader>tl)
-vim.api.nvim_set_keymap(
-	"n",
-	"<leader>tl",
-	":lua Toggle_linting()<CR>",
-	{ desc = "[T]oggle [L]inting", noremap = true, silent = true }
-)
+local function set_keymaps()
+	vim.keymap.set("n", "<leader>tl", function()
+		local lint = require("lint")
+		for _, linter_name in ipairs(lint.get_running(vim.api.nvim_get_current_buf())) do
+			local namespace = lint.get_namespace(linter_name)
+			vim.diagnostic.config({ virtual_text = false }, namespace)
+		end
+		-- vim.api.nvim_del_autocmd(vim.api.nvim_get_autocmds({ group = "lint" })[1].id)
+	end, { desc = "[T]oggle [L]inting", noremap = true, silent = true })
+end
 
-local linting_enabled = false
-
+-- WARN: doesn't work properly
 function Toggle_linting()
 	linting_enabled = not linting_enabled -- Toggle the state
 	if linting_enabled then
@@ -26,26 +29,41 @@ function Toggle_linting()
 	end
 end
 
--- return {}
-
 return {
-	{ "mfussenegger/nvim-lint" },
 	{
-		"rshkarin/mason-nvim-lint",
+		"mfussenegger/nvim-lint",
+		event = { "BufReadPre", "BufNewFile" },
 		dependencies = {
-			"williamboman/mason.nvim",
-			"mfussenegger/nvim-lint",
+			{
+				-- NOTE: "mason-nvim-lint" is used to install and eanble linters
+				"rshkarin/mason-nvim-lint",
+				dependencies = { "williamboman/mason.nvim" },
+				config = function()
+					-- 2)
+					-- Setup download of linters with "mason-nvim-lint"
+					require("mason-nvim-lint").setup({
+						ensure_installed = {
+							"golangcilint", -- name 'golangci-lint' can't be used for installation
+							"shellcheck", -- bash
+							-- "pylint",
+							-- "selene",
+							-- "luacheck", --WARNING: "luacheck" requires "luarocks" package maanger for lua
+						},
+						automatic_installation = true,
+						quiet_mode = true, -- Don't show notifications on startup
+					})
+				end,
+			},
 		},
 		config = function()
-			-- 1)
 			-- Confgiure linters with "nvim-lint"
-			-- WARNING: "nvim-lint" doesn't have 'setup' command!
+			-- WARNING: "nvim-lint" doesn't have 'require("lint").setup({...})' command!
 			-- Take a look at the source code!
 			require("lint").linters_by_ft = {
 				-- python = { "pylint" },
 				-- lua = { "selene" },
 				-- lua = { "luacheck" }, -- WARNING: "luacheck" requires "luarocks" package maanger for lua
-				-- go = { "golangcilint" },
+				go = { "golangcilint" },
 				-- NOTE: Below is default from nvim-lint
 				-- (/home/user/.local/share/balding-nvim/lazy/nvim-lint/lua/lint.lua)
 				text = { "vale" },
@@ -53,26 +71,14 @@ return {
 				markdown = { "vale" },
 				dockerfile = { "hadolint" },
 				terraform = { "tflint" },
+				sh = { "shellcheck" },
 			}
 
-			-- 2)
-			-- Setup download of linters with "mason-nvim-lint"
-			require("mason-nvim-lint").setup({
-				ensure_installed = {
-					-- "golangcilint", -- 'golangci-lint' can't be used for installation
-					-- "pylint",
-					-- "selene",
-					-- "luacheck", --WARNING: "luacheck" requires "luarocks" package maanger for lua
-				},
-				automatic_installation = true,
-				quiet_mode = true, -- Don't show notifications on startup
-			})
-
-			-- 3)
+			set_keymaps()
 			-- Setup an autocommand to trigger linting (https://github.com/mfussenegger/nvim-lint)
 			local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
-			vim.api.nvim_create_autocmd({ "BufWritePost", "InsertLeave", "TextChanged" }, {
+			vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave", "TextChanged" }, {
 				group = lint_augroup,
 				callback = function()
 					require("lint").try_lint()
