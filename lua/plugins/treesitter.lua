3d1843b956b97221b860a44b507268ed719ee4e9
cyrillus <kirill.olegovich31@gmail.com>
2025-07-07 11:18:41 +0300

    plugin(html): add ts-autotag for html tag completion and add default treesitter configs

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -8,13 +8,26 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
-		branch = 'master',
+		branch = "master",
 		lazy = false,
 		build = ":TSUpdate",
 		config = function()
-			require('nvim-treesitter.configs').setup {
+			require("nvim-treesitter.configs").setup({
 				-- A list of parser names, or "all" (the listed parsers MUST always be installed)
-				ensure_installed = { "go", "python", "c", "javascript", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+				ensure_installed = {
+					"go",
+					"python",
+					"c",
+					"cpp",
+					"javascript",
+					"lua",
+					"vim",
+					"vimdoc",
+					"query",
+					"markdown",
+					"markdown_inline",
+					"html",
+				},
 
 				-- Install parsers synchronously (only applied to `ensure_installed`)
 				sync_install = false,
@@ -52,7 +65,7 @@ return {
 					-- Instead of true it can also be a list of languages
 					additional_vim_regex_highlighting = false,
 				},
-			}
-		end
+			})
+		end,
 	},
 }
---
+++ lua/plugins/ts-autotag.lua
@@ -0,0 +1,20 @@
+return {
+	{
+		"windwp/nvim-ts-autotag",
+		config = function()
+			require("nvim-ts-autotag").setup({
+				opts = {
+					-- Defaults
+					enable_close = true, -- Auto close tags
+					enable_rename = true, -- Auto rename pairs of tags
+					enable_close_on_slash = false, -- Auto close on trailing </
+				},
+				aliases = {
+					markdown = "html",
+					markdown_inline = "html",
+					md = "html",
+				},
+			})
+		end,
+	},
+}
