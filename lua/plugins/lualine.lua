cf2251ebe3fd0a7cb3cf9ab711c7f1122b78c2e1
kfedtsov <kfedtsov@yandex-team.ru>
2025-08-07 16:07:39 +0300

    lualine(filepath): show full relative filepath istead of just parent

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -65,7 +65,7 @@ return {
 			},
 			sections = {
 				lualine_a = { { "mode", separator = { left = "" } } },
-				lualine_b = { "branch", { "filename", path = 4, shorting_target = 40 } },
+				lualine_b = { "branch", { "filename", path = 1, shorting_target = 10 } },
 				lualine_c = { "diff", "diagnostics" },
 				lualine_x = { "fileformat", "filetype", get_python_env },
 				lualine_y = { "lsp_status" },
