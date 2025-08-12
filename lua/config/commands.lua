54cf519eaf346771737ac58e3abae1582db4f844
kfedtsov <kfedtsov@yandex-team.ru>
2025-08-12 16:36:49 +0300

    resize_mode: add constant mode message

--- lua/config/commands.lua
+++ lua/config/commands.lua
@@ -15,12 +15,14 @@ function ResizeModeToggle()
 	if not resize_mode then
 		-- Enter resize mode: set global keymaps for h/j/k/l
 		resize_mode = true
-		vim.notify("Resize Mode: ON (h/j/k/l to resize, <Esc> to exit)", vim.log.levels.INFO)
-		vim.keymap.set("n", "h", ":vertical resize -2<CR>", { desc = "Resize left" })
-		vim.keymap.set("n", "l", ":vertical resize +2<CR>", { desc = "Resize right" })
-		vim.keymap.set("n", "j", ":resize -1<CR>", { desc = "Resize down" })
-		vim.keymap.set("n", "k", ":resize +1<CR>", { desc = "Resize up" })
+		RESIZE_MOD_NOTIFICATION = "Resize Mode: ON (h/j/k/l to resize, <Esc> or q to exit)"
+		vim.notify(RESIZE_MOD_NOTIFICATION, vim.log.levels.INFO)
+		vim.keymap.set("n", "h", ":vertical resize -2<CR>:lua print(RESIZE_MOD_NOTIFICATION)<CR>", { desc = "Resize left" })
+		vim.keymap.set("n", "l", ":vertical resize +2<CR>:lua print(RESIZE_MOD_NOTIFICATION)<CR>", { desc = "Resize right" })
+		vim.keymap.set("n", "j", ":resize -1<CR>:lua print(RESIZE_MOD_NOTIFICATION)<CR>", { desc = "Resize down" })
+		vim.keymap.set("n", "k", ":resize +1<CR>:lua print(RESIZE_MOD_NOTIFICATION)<CR>", { desc = "Resize up" })
 		vim.keymap.set("n", "<Esc>", ResizeModeToggle, { desc = "Exit Resize Mode" })
+		vim.keymap.set("n", "q", ResizeModeToggle, { desc = "Exit Resize Mode" })
 	else
 		-- Exit resize mode: remove global keymaps for h/j/k/l
 		resize_mode = false
@@ -30,6 +32,8 @@ function ResizeModeToggle()
 		vim.keymap.del("n", "j")
 		vim.keymap.del("n", "k")
 		vim.keymap.del("n", "<Esc>")
+		vim.keymap.del("n", "q")
+		RESIZE_MOD_NOTIFICATION = ""
 	end
 end
 
