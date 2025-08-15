5115cfd2982e6bc1532d290f27a3dcf84e175130
kfedtsov <kfedtsov@yandex-team.ru>
2025-08-15 11:12:37 +0300

    docs: add explanations on why there's a change in pyright roots sequnce

--- init.lua
+++ init.lua
@@ -11,6 +11,8 @@ require("config.keymappings")
 require("config.diagnostic")
 
 
+-- change what defines root markers for pyright
+-- Logic: if the first root marker is not found anywhere down to the root, it looks for the next marker after that
 vim.lsp.config("pyright", {
 	root_markers = {  "pyrightconfig.json", "pyproject.toml", "setup.py", "setup.cfg", "requirements.txt", "Pipfile", ".git" }
 })
