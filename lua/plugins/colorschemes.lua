9fe5581f84af4a9eb4846be2fd0553ac84f1359b
kfedtsov <kfedtsov@yandex-team.ru>
2025-08-28 18:00:20 +0300

    colorschemes: add some colorschemes and change current

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,7 +1,7 @@
 return {
 	{
 		"EdenEast/nightfox.nvim",
-		enabled = true,
+		enabled = false,
 		config = function()
 			require("nightfox").setup({
 				options = {
@@ -10,10 +10,9 @@ return {
 			})
 			-- Dark themes:
 			-- vim.cmd.colorscheme("terafox")
-			-- vim.cmd.colorscheme("nightfox")
+			vim.cmd.colorscheme("nightfox")
 			-- vim.cmd.colorscheme("duskfox")
-			-- vim.cmd.colorscheme("duskfox")
-			vim.cmd.colorscheme("nordfox")
+			-- vim.cmd.colorscheme("nordfox")
 			-- vim.cmd.colorscheme("carbonfox")
 			--
 			-- Light themes:
@@ -42,13 +41,35 @@ return {
 	},
 	{
 		"rebelot/kanagawa.nvim",
-		enabled = false,
+		enabled = true,
 		lazy = false,
 		config = function()
 			require("kanagawa").setup({
 				dimInactive = true,
+				compile = false,             -- enable compiling the colorscheme
+				undercurl = true,            -- enable undercurls
+				commentStyle = { italic = true },
+				functionStyle = {},
+				keywordStyle = { italic = true},
+				statementStyle = { bold = true },
+				typeStyle = {},
+				transparent = false,         -- do not set background color
+				terminalColors = true,       -- define vim.g.terminal_color_{0,17}
+				colors = {                   -- add/modify theme and palette colors
+						palette = {},
+						theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+				},
+				overrides = function(colors) -- add/modify highlights
+						return {}
+				end,
+				theme = "wave",              -- Load "wave" theme
+				background = {               -- map the value of 'background' option to a theme
+						dark = "wave",           -- wave | dragon | lotus
+						light = "lotus"
+				},
 			})
-			vim.cmd.colorscheme("kanagawa-wave")
+			-- vim.cmd.colorscheme("kanagawa-wave")
+			vim.cmd.colorscheme("kanagawa")
 		end,
 	},
 	{
@@ -66,7 +87,27 @@ return {
 		lazy = false,
 		name = "github-theme",
 		config = function()
-			vim.cmd.colorscheme("github_dark_dimmed")
+			require("github-theme").setup({
+				options = {
+					dim_inactive = true,
+				}
+			})
+			-- vim.cmd.colorscheme("github_dark_dimmed")
+			vim.cmd.colorscheme("github_dark")
 		end,
 	},
+	{
+			"scottmckendry/cyberdream.nvim",
+			enabled = false,
+			lazy = false,
+			priority = 1000,
+			config = function ()
+				require("cyberdream").setup({
+					variant = "default", -- light | auto
+					transparent = true,
+					italic_comments = true,
+				})
+				vim.cmd.colorscheme("cyberdream")
+			end
+	}
 }
