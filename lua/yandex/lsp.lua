f44cbb11781ca83f982226b2c55da329a714078e
kfedtsov <kfedtsov@yandex-team.ru>
2025-09-29 13:13:48 +0300

    fix(pyright): specify yapath in venv as python binary

--- lua/yandex/lsp.lua
+++ lua/yandex/lsp.lua
@@ -8,4 +8,16 @@ vim.lsp.config("gopls", {
 	},
 })
 
+-- change what defines root markers for pyright
+-- Logic: if the first root marker is not found anywhere down to the root, it looks for the next marker after that
+vim.lsp.config("pyright", {
+	root_markers = {  "pyrightconfig.json", "pyproject.toml", "setup.py", "setup.cfg", "requirements.txt", "Pipfile", ".git" },
+	settings = {
+		python = {
+			pythonPath = "~/.venvs/taxi-python/bin",
+			autoSearchPaths = false,
+		}
+	}
+})
+
 return { {} }
