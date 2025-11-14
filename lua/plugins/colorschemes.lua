6912c125b94d1feaadffe1d970c0acd23611562e
kfedtsov <kfedtsov@yandex-team.ru>
2025-11-14 16:12:36 +0300

    fonts: add italics to nightfox

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,3 +1,29 @@
+local different_styles = {
+	default = {
+		comments = "NONE",
+		conditionals = "NONE",
+		constants = "NONE",
+		functions = "NONE",
+		keywords = "NONE",
+		numbers = "NONE",
+		operators = "NONE",
+		strings = "NONE",
+		types = "NONE",
+		variables = "NONE",
+	},
+	first = {
+		comments = "italic", -- Dim and italic = unobtrusive
+		conditionals = "italic", -- Subtle flow hints
+		constants = "bold", -- Stand out without shouting
+		functions = "bold", -- Find function names quickly
+		keywords = "italic", -- Separate structure words
+		numbers = "NONE", -- Color is enough
+		operators = "NONE", -- Don't distract
+		strings = "NONE", -- Let color do the work
+		types = "italic", -- Distinguish classic and user-defined types
+		variables = "NONE", -- Default for easy reading
+	},
+}
 return {
 	{
 		"EdenEast/nightfox.nvim",
@@ -6,6 +32,7 @@ return {
 			require("nightfox").setup({
 				options = {
 					dim_inactive = true,
+					styles = different_styles["first"],
 				},
 			})
 			-- Dark themes:
@@ -46,26 +73,26 @@ return {
 		config = function()
 			require("kanagawa").setup({
 				dimInactive = true,
-				compile = false,             -- enable compiling the colorscheme
-				undercurl = true,            -- enable undercurls
+				compile = false, -- enable compiling the colorscheme
+				undercurl = true, -- enable undercurls
 				commentStyle = { italic = true },
 				functionStyle = {},
-				keywordStyle = { italic = true},
+				keywordStyle = { italic = true },
 				statementStyle = { bold = true },
 				typeStyle = {},
-				transparent = false,         -- do not set background color
-				terminalColors = true,       -- define vim.g.terminal_color_{0,17}
-				colors = {                   -- add/modify theme and palette colors
-						palette = {},
-						theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+				transparent = false, -- do not set background color
+				terminalColors = true, -- define vim.g.terminal_color_{0,17}
+				colors = { -- add/modify theme and palette colors
+					palette = {},
+					theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
 				},
 				overrides = function(colors) -- add/modify highlights
-						return {}
+					return {}
 				end,
-				theme = "wave",              -- Load "wave" theme
-				background = {               -- map the value of 'background' option to a theme
-						dark = "wave",           -- wave | dragon | lotus
-						light = "lotus"
+				theme = "wave", -- Load "wave" theme
+				background = { -- map the value of 'background' option to a theme
+					dark = "wave", -- wave | dragon | lotus
+					light = "lotus",
 				},
 			})
 			-- vim.cmd.colorscheme("kanagawa-wave")
@@ -90,24 +117,24 @@ return {
 			require("github-theme").setup({
 				options = {
 					dim_inactive = true,
-				}
+				},
 			})
 			-- vim.cmd.colorscheme("github_dark_dimmed")
 			vim.cmd.colorscheme("github_dark")
 		end,
 	},
 	{
-			"scottmckendry/cyberdream.nvim",
-			enabled = false,
-			lazy = false,
-			priority = 1000,
-			config = function ()
-				require("cyberdream").setup({
-					variant = "default", -- light | auto
-					transparent = true,
-					italic_comments = true,
-				})
-				vim.cmd.colorscheme("cyberdream")
-			end
-	}
+		"scottmckendry/cyberdream.nvim",
+		enabled = false,
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("cyberdream").setup({
+				variant = "default", -- light | auto
+				transparent = true,
+				italic_comments = true,
+			})
+			vim.cmd.colorscheme("cyberdream")
+		end,
+	},
 }
