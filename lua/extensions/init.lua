b4fe41aee563e01c622a71165ea7ddbf8b792cbd
cyrillus <kirill.olegovich31@gmail.com>
2026-04-22 22:21:50 +0300

    Remove empty extensions/ directory

--- lua/extensions/init.lua
+++
@@ -1,7 +0,0 @@
--- Extensions - non-Yandex specific tools and configurations
--- This module provides a way to add functionality without Yandex dependencies
--- It is loaded unconditionally and serves as a container for external tool integrations
-
-local M = {}
-
-return M
